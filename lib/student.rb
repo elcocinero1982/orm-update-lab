28efea89c943a60a11edad38e8a10258e7c6aedd
jose luis <cortezluisjose@gmail.com>
2019-10-12 03:51:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,22 +42,22 @@ student.save
 student
 end 
 def self.new_from_db(row)
-  id = row[0]
+   id = row[0]
 
-  name = row[1]
+   name = row[1]
 
-  grade = row[2]
+   grade = row[2]
 
-  self.new(id, name, grade)
+   self.new(id, name, grade)
 
 end
 def self.find_by_name(name)
-  
-end
-	 def update
-	 sql = "UPDATE songs SET name = ?, album = ? WHERE id = ?"
-	 DB[:conn].execute(sql, self.name, self.album, self.id)
-  end
-	
+   sql = "SELECT * FROM students WHERE name = ?"
+
+   DB[:conn].execute(sql, name).map { |row| new_from_db(row) }.first
 
 end
+
+def update
+
+
